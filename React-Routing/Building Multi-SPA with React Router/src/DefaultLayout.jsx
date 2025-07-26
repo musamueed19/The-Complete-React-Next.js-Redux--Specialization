0be730220245d52a96255640e52a664f20961cdd
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-07-26 10:09:46 +0500

    added styling, and link to products in default lyout

--- React-Routing/Building Multi-SPA with React Router/src/DefaultLayout.jsx
+++ React-Routing/Building Multi-SPA with React Router/src/DefaultLayout.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const DefaultLayout = () => {
   return (
-    <div>DefaultLayout
-
+    <div className="w-[90%] mx-auto text-xl space-y-2">
+      <h1>Default Layout</h1>
+      <div className="">
+        <Link
+          to={"/products"}
+          className="px-2 py-1 rounded-lg bg-gray-100 font-medium"
+        >
+          Products
+        </Link>
+      </div>
       {/* children */}
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default DefaultLayout
+export default DefaultLayout;
