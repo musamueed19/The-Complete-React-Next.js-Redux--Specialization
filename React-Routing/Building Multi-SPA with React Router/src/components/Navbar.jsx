3fd20a283a1018426621b6024eed8e78126a1e8e
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-07-26 19:01:05 +0500

    update Active NavLink

--- React-Routing/Building Multi-SPA with React Router/src/components/Navbar.jsx
+++ React-Routing/Building Multi-SPA with React Router/src/components/Navbar.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
-
-import {navItems} from '../constants/constant'
+import { navItems } from "../constants/constant";
 
 const Navbar = () => {
   return (
-      <nav>
-          <div className='flex gap-x-3 md:gap-x-6'>
-              {
-                  navItems?.map((item, index) => (
-                      <Link key={index} to={item.to} className='navLink'>{item.title}</Link>
-                  ))
-              }
-              
-        </div>
-          
+    <nav>
+      <div className="flex gap-x-3 md:gap-x-6">
+        {navItems?.map((item, index) => (
+          <NavLink
+            key={index}
+            to={item.to}
+            className={({ isActive }) =>
+              isActive
+                ? "border-amber-600 border-b-4 font-semibold navLink"
+                : "navLink hover:text-amber-500 hover:underline underline-offset-9"
+            }
+            end
+          >
+            {item.title}
+          </NavLink>
+        ))}
+      </div>
     </nav>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
